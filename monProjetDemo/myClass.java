2ede987d2e39860384e21adb13dc75e73cde7cc2
said moutaa <smoutaa@gmail.com>
2018-12-10 21:33:14 +0000

    adding a new line of code

--- monProjetDemo/myClass.java
+++ monProjetDemo/myClass.java
@@ -3,6 +3,7 @@ class myClass{
   public static void main(String[] args){ 
    
 	System.out.println("This is just a demo class file");
+	System.out.println("This class literally does nothing");
 
   }
 
